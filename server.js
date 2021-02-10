62a40a0b84bb44d759e234785fa36a2b415050a1
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-02-10 14:30:09 +0100

    Added error handler for server

--- server.js
+++ server.js
@@ -19,6 +19,39 @@ const normalizePort = (value) => {
 const PORT = normalizePort(process.env.PORT || '3000');
 app.set('port', PORT);
 
+const getBind = (address) => {
+  return typeof address === 'string' ? 'pipe ' + address : 'port: ' + PORT;
+}
+
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const address = server.address();
+  const bind = getBind(address);
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' required elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
 const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = getBind(address);
+  console.log('Listening on ' + bind)
+})
 
 server.listen(PORT);
