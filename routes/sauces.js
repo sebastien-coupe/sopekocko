8a66466fab6019c242dd361ce02565fa23ab4c83
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-02-10 18:05:48 +0100

    Added authentication middleware

--- routes/sauces.js
+++ routes/sauces.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = require('../app');
 const router = express.Router();
 
+const auth = require('../middlewares/authMiddleware');
+
 const sauceController = require('../controllers/sauceController');
 
-router.get('/', sauceController.getAll);
-router.get('/:id', sauceController.getOne);
-router.post('/', sauceController.create);
-router.put('/:id', sauceController.update);
-router.delete('/:id', sauceController.delete);
+router.get('/', auth, sauceController.getAll);
+router.get('/:id', auth, sauceController.getOne);
+router.post('/', auth, sauceController.create);
+router.put('/:id', auth, sauceController.update);
+router.delete('/:id', auth, sauceController.delete);
 
 module.exports = router;
