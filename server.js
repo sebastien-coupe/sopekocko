ac3a3425cf1b3cda5807eee504add75eb3c0b39e
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-02-10 14:16:47 +0100

    Added PORT normalization

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const http = require('http');
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
 
+const normalizePort = (value) => {
+  const port = parseInt(value, 10);
+
+  if (isNaN(port)) {
+    return value;
+  }
+
+  if (port >= 0) {
+    return port
+  }
+
+  return false
+
+}
+
+const PORT = normalizePort(process.env.PORT || '3000');
 app.set('port', PORT);
 
 const server = http.createServer(app);
