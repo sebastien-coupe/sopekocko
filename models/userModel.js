451a05293ffee066545e5bf239cc14b415011298
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-02-10 15:26:30 +0100

    Added model for user

---
+++ models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  userId: {
+    type: ObjectId
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema)
