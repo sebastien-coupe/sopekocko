6a0c85c6f1e467f8046d0ea8a5398db4e0b43d9b
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-02-10 14:37:41 +0100

    Set headers to prevent cors errors

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// Prevent Cors Errors
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTION');
+  next();
+})
+
 app.use((req, res) => {
   res.json({
     message: "All is working!"
