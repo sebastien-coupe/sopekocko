2cfff5399dfabe8ff9fee7770913842af9db9f0f
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-02-24 12:08:09 +0100

    Fixed formatting

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,11 +52,7 @@ exports.login = (req, res, next) => {
             )
           })
         })
-        .catch(error => {
-          res.status(500).json({ error })
-        })
+        .catch(error => res.status(500).json({ error }))
     })
-    .catch(error => {
-      res.status(500).json({ error })
-    })
+    .catch(error => res.status(500).json({ error }))
 }
