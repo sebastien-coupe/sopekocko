e9a393948933dfcc30907d27520cd404c33dc160
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-02-25 16:12:35 +0100

    Added express-rate-limit module to prevent bruteforce attacks

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
+
+// Limit number of request to login
+const limiter = rateLimit({
+  windowMs: 30 * 60 * 1000,
+  max: 100
+});
 
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/login', limiter, authController.login);
 
 module.exports = router;
